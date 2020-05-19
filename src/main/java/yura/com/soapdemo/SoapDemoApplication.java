e0ad888ae360b06143babf7ecc02d02eeb4ca29a
Yura <yuriy.petrashenko7@gmail.com>
2020-05-19 16:09:25 +0300

    Initial commit

---
+++ src/main/java/yura/com/soapdemo/SoapDemoApplication.java
@@ -0,0 +1,13 @@
+package yura.com.soapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yura/com/soapdemo/SoapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package yura.com.soapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
